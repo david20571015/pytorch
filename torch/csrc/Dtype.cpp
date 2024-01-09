c6280d0d6ae7206f29851f41b0a36738754e13e5
Jerry Zhang <jerryzh168@gmail.com>
2024-01-08 16:43:06 -0800

    Add support for instantiating a new torch.dtype in python
    Summary:
    This allows us to add new custom dtypes in the python frontend such as torch.int4,
    it will be used in https://github.com/pytorch-labs/ao/pull/13
    
    Test Plan:
    python test/test_quantization.py -k test_new_dtype
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 3df771570b8ec346799f58eb914fe42071bbba32
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/116294

--- torch/csrc/Dtype.cpp
+++ torch/csrc/Dtype.cpp
@@ -4,7 +4,10 @@
 #include <structmember.h>
 #include <torch/csrc/DynamicTypes.h>
 #include <torch/csrc/Exceptions.h>
+#include <torch/csrc/THP.h>
 #include <torch/csrc/utils/object_ptr.h>
+#include <torch/csrc/utils/pycfunction_helpers.h>
+#include <torch/csrc/utils/python_arg_parser.h>
 #include <torch/csrc/utils/python_numbers.h>
 #include <torch/csrc/utils/python_strings.h>
 #include <torch/csrc/utils/tensor_dtypes.h>
@@ -22,6 +25,19 @@ PyObject* THPDtype_New(at::ScalarType scalar_type, const std::string& name) {
   self_->scalar_type = scalar_type;
   std::strncpy(self_->name, name.c_str(), DTYPE_NAME_LEN);
   return self.release();
+  END_HANDLE_TH_ERRORS
+}
+
+PyObject* THPDtype_pynew(PyObject* type, PyObject* args, PyObject* kwargs) {
+  HANDLE_TH_ERRORS
+  static torch::PythonArgParser parser(
+      {"_custom_dtype(ScalarType scalartype, c10::string_view name)"});
+  torch::ParsedArgs<2> parsed_args;
+  auto r = parser.parse(args, kwargs, parsed_args);
+  if (r.idx == 0) {
+    return THPDtype_New(r.scalartype(0), r.string(1));
+  }
+  Py_RETURN_NONE;
   END_HANDLE_TH_ERRORS
 }
 
@@ -156,6 +172,16 @@ PyTypeObject THPDtypeType = {
     nullptr, /* tp_alloc */
     nullptr, /* tp_new */
 };
+
+static PyMethodDef TorchMethods[] = { // NOLINT
+  {"_custom_dtype",
+   castPyCFunctionWithKeywords(THPDtype_pynew),
+   METH_VARARGS | METH_KEYWORDS,
+   nullptr},
+};
+
+static std::vector<PyMethodDef> methods;
+
 
 void THPDtype_init(PyObject* module) {
   // Set a __dict__ with `__module__` = `torch`. This means
